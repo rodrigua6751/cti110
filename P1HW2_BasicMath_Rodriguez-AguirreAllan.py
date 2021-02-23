2bd507ed6e62d9e3a6698e784ee30e702bcaecf2
rodrigua6751 <78821417+rodrigua6751@users.noreply.github.com>
2021-02-23 11:51:27 -0500

    Add files via upload
    Fifth submission (IDLE-Math)

---
+++ P1HW2_BasicMath_Rodriguez-AguirreAllan.py
@@ -0,0 +1,21 @@
+#User input oriented Basic Math
+#2/23/21
+#CTI-110 P1HW2 - Basic Math
+#Allan Rodriguez-Aguirre
+
+first_num = int(input('Enter the first number: '))
+sec_num = int(input('Enter the second number: '))
+sum = first_num + sec_num
+product = first_num * sec_num
+print('First number entered:', first_num)
+print('Second number entered:', sec_num)
+print()
+print('Sum of both numbers:', sum)
+print('Product of the two numbers:', product)
+
+#Input first number
+#Input second number
+#Display first number
+#Display second number
+#Calculate sum of first and second number
+#Calculate multiplication product of first and second number
