034f7aede14ac96360731bc31857a5c09d464df9
rodrigua6751 <78821417+rodrigua6751@users.noreply.github.com>
2021-02-25 13:05:17 -0500

    Add files via upload
    Sixth submission (Divide by X)

---
+++ P2LAB1_DivideByX_Rodriguez-AguirreAllan.py
@@ -0,0 +1,9 @@
+second_int = ''
+user_num = int(input())
+x = int(input(second_int))
+user_num = user_num // x
+print(user_num, end=' ')
+user_num = user_num // x
+print(user_num, end=' ')
+user_num = user_num // x
+print(user_num)
