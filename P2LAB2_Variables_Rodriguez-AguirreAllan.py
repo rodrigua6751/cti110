8787db2c138cccd66e4a0bb15aae5564a484bc0a
rodrigua6751 <78821417+rodrigua6751@users.noreply.github.com>
2021-03-02 14:53:05 -0500

    Add files via upload
    Seventh Submission (Variables)

---
+++ P2LAB2_Variables_Rodriguez-AguirreAllan.py
@@ -0,0 +1,6 @@
+user_int = int(input('Enter integer (32 - 126):\n'))
+user_float = float(input('Enter float:\n'))
+user_character = str(input('Enter character:\n'))
+user_string = str(input('Enter string:\n'))
+print(user_int, user_float, user_character, user_string+'\n'+user_string, user_character, user_float, user_int)
+print(user_int, 'converted to a character is', chr(user_int))
