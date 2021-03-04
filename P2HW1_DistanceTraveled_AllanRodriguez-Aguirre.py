a56d7f85c215e2ff7141fce27e72c540f0ca0b73
rodrigua6751 <78821417+rodrigua6751@users.noreply.github.com>
2021-03-04 01:06:18 -0500

    Add files via upload
    Eight Submission (Distance Traveled)

---
+++ P2HW1_DistanceTraveled_AllanRodriguez-Aguirre.py
@@ -0,0 +1,20 @@
+#Calculation of Distance
+#03/04/21
+#CTI-110 P2HW1 - DistanceTraveled
+#Allan Rodriguez-Aguirre
+
+
+first_num = float(input('Enter car\'s speed: '))
+sec_num = float(input('Enter time traveled: '))
+product = first_num * sec_num
+print()
+print('Speed entered:', first_num)
+print('Time entered:', sec_num)
+print()
+print('Distance Traveled', product, 'miles')
+
+#Input first number indicating speed
+#Input second number indicating time
+#Display first number
+#Display second number
+#Calculate product of first and second number when multiplied
